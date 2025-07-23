2a96619dacbe56e0399ff79b153e7cc470f66b39
Semanur Gökırmak <154887690+semanurgokirmak@users.noreply.github.com>
2025-07-24 02:03:15 +0300

    4. commit

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
 
 // Veritabanı bağlantısı
 const db = new sqlite3.Database('./kelimeler.db');
